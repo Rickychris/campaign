9857d194d14a16132c72f4a3faa899406a8ff358
Ricky <ricky.pinto@moonraft.com>
2021-07-19 18:50:27 +0530

    environment variables added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase";
 // Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJz8ICB5pwPCT701Q4G7TsGc8gFIFioBI",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "marketing-campaign-1d550.firebaseapp.com",
   projectId: "marketing-campaign-1d550",
   storageBucket: "marketing-campaign-1d550.appspot.com",
-  messagingSenderId: "690423576368",
-  appId: "1:690423576368:web:a1513fa83a5137f097faf5",
+  messagingSenderId: process.env.MESSAGING_SENDERP_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
